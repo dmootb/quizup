71a2d90ef9b17e244531e5ed190c3823217fd918
dmootb <srmootdev@gmai.com>
2024-01-03 21:38:09 -0600

    api works

--- web/core/api/v1/urls.py
+++ web/core/api/v1/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import SimpleRouter
 from web.core.api.v1.views.core import QuestionViewSet
 
 router = SimpleRouter()
-router.register(r'questions', QuestionViewSet, basename="api/core/v1/questions")
-
+router.register(r"question", QuestionViewSet, basename="api/core/v1/question")
+print(router.urls)
 urlpatterns = [
     re_path(r"^", include(router.urls)),
 ]
--- web/core/tests/api/v1/views/test_core.py
+++ web/core/tests/api/v1/views/test_core.py
@@ -1,25 +1,23 @@
-# web/core/tests/api/v1/views/test_views.py
-
+import json
 from typing import ClassVar, Optional, Type
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status, viewsets
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from web.core.api.v1.views.core import QuestionViewSet
 from web.core.utils.data import Creator
 
-class QuestionViewSetTests(TestCase):
-    named_view_base: ClassVar[str] = "api/v1/core/questions/"
+class QuestionViewSetTests(APITestCase):
+    named_view_base: ClassVar[str] = "api/core/v1/question"
     namespace: ClassVar[Optional[str]] = None
     viewset: ClassVar[Type[viewsets.ModelViewSet]] = QuestionViewSet
 
     @classmethod
     def setUpClass(cls) -> None:
-        import django
-        django.setup()
+        super().setUpClass()
         cls.client = APIClient()
         cls.creator = Creator()
 
+    
     def test_get_questions_with_answers(self):
         # Create a sample question with answers
         question = self.creator.create_question(question_text="Sample Question", category="General")
@@ -28,12 +26,12 @@ class QuestionViewSetTests(TestCase):
         self.creator.create_answer(question, answer_text="Option 3", is_correct=False)
         self.creator.create_answer(question, answer_text="Option 4", is_correct=False)
 
-        # Get the list of questions with answers
-        url = reverse(self.named_view_base, current_app=self.namespace)
+        url = reverse(self.named_view_base + "-list", current_app=self.namespace)
         print(url)  # Assuming you have a 'question-list' URL in your QuestionViewSet
         response = self.client.get(url)
-        print(response)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # self.assertEqual(len(response.data), 1)  # Assuming you expect one question in the response
-        # self.assertEqual(len(response.data[0]['answers']), 4)  # Assuming you expect four answers for each question
+        resJson = json.loads(response.content)
+        print(resJson)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)  # Assuming you expect one question in the response
+        self.assertEqual(len(response.data[0]['answers']), 4)  # Assuming you expect four answers for each question
         # Add more assertions as needed
